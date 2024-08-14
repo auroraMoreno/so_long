4fda9ae98e2a10d282ec4e9e3bd5bfe345dae348
Aurora Moreno <aurora.moreno.lopez13@gmail.com>
2024-08-14 19:53:43 +0200

    pinta una imagen y cierra la ventana, intentar pintar por capas ahora

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 08:04:37 by aumoreno          #+#    #+#             */
-/*   Updated: 2024/08/07 14:22:21 by aumoreno         ###   ########.fr       */
+/*   Updated: 2024/08/14 19:43:27 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 /**ACORDARSE DE PONER EL WALL DE VUELTA EN EL MAKEFILE!!!!!!!!!! */
 
+// function to push pixel to an image 
+// replaces the mlx_pixel_put as this one is slower
+// this is bc it pushes px instantly into the window 
+// without waiting for the frame being enitrely rendered 
+// this is why we buffer our px to the img and then push it to the window 
 void ft_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst;
@@ -22,19 +27,35 @@ void ft_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int ft_close_window(mlx_data *data)
+{
+	mlx_destroy_window(data->mlx, data->mlx_win);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	exit(0);
+	return (0);
+}
+
 int main(void)
 {
-	void *mlx;
-	void *mlx_win;
-	//void *img;
-	t_data img;
 
-	mlx = mlx_init(); // a connection to the correct graphical system
-	img.img = mlx_new_image(mlx,1920,1080); // init an image 
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "hi hiii"); //this creates the window
-	img.addr = mlx_get_data_addr(img.img, &img.bit_per_pixel, &img.line_len, &img.endian);
-	ft_mlx_pixel_put(&img, 5, 5,0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx); //initiates thw window rendering 
+	t_data img;
+	img_data img_data;
+	mlx_data mlx_data;
+
+	img_data.path = "textures/test.xpm";
+	mlx_data.mlx = mlx_init(); // a connection to the correct graphical system
+	
+	img.img_ptr = mlx_xpm_file_to_image(mlx_data.mlx, img_data.path, &img_data.width, &img_data.height);
+
+	mlx_data.mlx_win = mlx_new_window(mlx_data.mlx, 780, 480, "hi hiii"); //this creates the window
+	img.addr = mlx_get_data_addr(img.img_ptr, &img.bit_per_pixel, &img.line_len, &img.endian);
+
+	mlx_put_image_to_window(mlx_data.mlx, mlx_data.mlx_win, img.img_ptr, 0, 0);
+	
+	mlx_hook(mlx_data.mlx_win,17,1L<<17,&ft_close_window,&mlx_data);
+
+	mlx_loop(mlx_data.mlx); //initiates the window rendering 
+	
 	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -6,17 +6,36 @@
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 08:05:39 by aumoreno          #+#    #+#             */
-/*   Updated: 2024/08/07 14:08:22 by aumoreno         ###   ########.fr       */
+/*   Updated: 2024/08/14 19:13:59 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx/mlx.h"
 #include <stdlib.h>
+#include "libft/libft.h"
+#include "ft_printf/ft_printf.h"
+
+
+typedef struct mlx_data
+{
+    void *mlx;
+    void *mlx_win; 
+}mlx_data;
+
 
 typedef struct s_data {
-    void *img;
+    void *img_ptr;
     char *addr;
     int bit_per_pixel;
     int line_len;
     int endian;
 } t_data;
+
+
+typedef struct img_data {
+    void *img_name;
+    int   width;
+    int    height;
+    char   *path;
+    
+} img_data;
