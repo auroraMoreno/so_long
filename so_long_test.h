082a4b3fb6001adeee6bbcbefe55cc814e346d93
Aurora Moreno <aurora.moreno.lopez13@gmail.com>
2024-09-03 07:30:06 +0200

    comienzo juego

--- so_long_test.h
+++ so_long_test.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/31 10:26:59 by aumoreno          #+#    #+#             */
-/*   Updated: 2024/09/02 12:23:23 by aumoreno         ###   ########.fr       */
+/*   Created: 2024/08/06 08:05:39 by aumoreno          #+#    #+#             */
+/*   Updated: 2024/08/27 12:10:46 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,45 +15,30 @@
 #include "libft/libft.h"
 #include "ft_printf/ft_printf.h"
 
-typedef struct s_mlx_data
-{
-    void *mlx;
-    void *mlx_win; 
-}t_mlx_data;
 
-typedef struct s_img {
+// lista de s_data
+
+typedef struct s_data {
     void *img_ptr;
     char *addr;
-    void *img_name;
-    int   width;
-    int   height;
-    char  *path;
     int bit_per_pixel;
     int line_len;
     int endian;
-    int x;
-    int y;
-} t_img;
+    //img_data data;
+} t_data;
 
-typedef struct s_game_data
-{
-    t_mlx_data mlx;
-    t_img floor;
-    t_img wall; 
+
+typedef struct img_data {
+    void *img_name;
+    int   width;
+    int    height;
+    char   *path;
     
-}t_game;
+} img_data;
 
-
-int ft_handle_key(int keycode, t_mlx_data *data);
-int ft_close_window(t_mlx_data *data);
-
-
-
-// typedef struct s_img_data {
-//     void *img_ptr;
-//     char *addr;
-//     int bit_per_pixel;
-//     int line_len;
-//     int endian;
-//     //img_data data;
-// } t_img_data;
+typedef struct mlx_data
+{
+    void *mlx;
+    void *mlx_win; 
+    t_data img_ptr; 
+}mlx_data;
